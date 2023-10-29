683815f8831084c2605a20caeabaf6b1631e0c53
mukesh <mukeshgogai6@gmail.com>
2023-10-29 22:50:46 +0530

    feat: created Navigation button component
    1. Added colors
    2. Added icon for NavigationButton

--- androidApp/src/main/java/com/spot/saver/android/MainActivity.kt
+++ androidApp/src/main/java/com/spot/saver/android/MainActivity.kt
@@ -3,12 +3,20 @@ package com.spot.saver.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.spot.saver.Greeting
+import com.spot.saver.android.theme.ActionColor
+import com.spot.saver.android.ui.NavigationButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +25,9 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.primary
                 ) {
-                    GreetingView(Greeting().greet())
+
                 }
             }
         }
@@ -35,6 +43,16 @@ fun GreetingView(text: String) {
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        Surface(
+            color = MaterialTheme.colorScheme.primary
+        ){
+            NavigationButton(
+                modifier =
+                Modifier.background(ActionColor, shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp, bottomStart = 10.dp, bottomEnd = 10.dp))
+                    .height(62.dp)
+                    .width(61.dp)
+            )
+        }
+
     }
 }
---
+++ androidApp/src/main/java/com/spot/saver/android/theme/Colors.kt
@@ -0,0 +1,14 @@
+package com.spot.saver.android.theme
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
+
+val ActionColor = Color(0xFF42677C)
+
+val DarkPrimaryColor = Color(0xFFBB86FC)
+val DarkSecondaryColor = Color(0xFF03DAC5)
+val DarkTertiaryColor = Color(0xFF3700B3)
+
+val LightPrimaryColor = Color(0xFF6200EE)
+val LightSecondaryColor = Color(0xFF03DAC5)
+val LightTertiaryColor = Color(0xFF3700B3)
