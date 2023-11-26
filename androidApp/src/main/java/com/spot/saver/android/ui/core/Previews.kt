d885040fec1b9b69287ae8dcda579874b6e08155
Pavitra <pareshraut50@gmail.com>
2023-11-26 06:42:12 +0530

    refactor: Segregate preview annotation for re-usability

--- androidApp/src/main/java/com/spot/saver/android/ui/core/Previews.kt
+++ androidApp/src/main/java/com/spot/saver/android/ui/core/Previews.kt
@@ -3,16 +3,22 @@ package com.spot.saver.android.ui.core
 import android.content.res.Configuration.*
 import androidx.compose.ui.tooling.preview.Preview
 
+@LightThemePreview
+@DarkThemePreview
+annotation class ThemedComponentPreviews
+
 @Preview(
     name = "Light Theme",
     group = "Component Theme",
     showBackground = true,
     uiMode = UI_MODE_NIGHT_NO or UI_MODE_TYPE_NORMAL
 )
+annotation class LightThemePreview
+
 @Preview(
     name = "Dark Theme",
     group = "Component Theme",
     showBackground = true,
     uiMode = UI_MODE_NIGHT_YES or UI_MODE_TYPE_NORMAL
 )
-annotation class ThemedComponentPreviews
+annotation class DarkThemePreview
