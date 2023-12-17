ed1f930e6a4fb7952554b9a499913d13713ef15f
Pavitra <pareshraut50@gmail.com>
2023-12-17 21:26:58 +0530

    Revert "feat: Enable Compose in Shared module"
    This reverts commit 493453452a77cc175675699195fb32ec1aff7408.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,4 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
-    alias(libs.plugins.composeMultiplatform).apply(false)
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.composeMultiplatform)
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
@@ -30,11 +29,6 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 //put your multiplatform dependencies here
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material)
-                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-                implementation(compose.components.resources)
             }
         }
         val commonTest by getting {
