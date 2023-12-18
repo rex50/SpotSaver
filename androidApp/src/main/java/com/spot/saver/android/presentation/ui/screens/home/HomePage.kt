e983b857f6be2827c8e0813ea7df1df2f8fa1bae
Pavitra <pareshraut50@gmail.com>
2023-12-19 00:55:55 +0530

    feat: sticky header with shadow

--- androidApp/src/main/java/com/spot/saver/android/presentation/ui/screens/home/HomePage.kt
+++ androidApp/src/main/java/com/spot/saver/android/presentation/ui/screens/home/HomePage.kt
@@ -1,5 +1,7 @@
 package com.spot.saver.android.presentation.ui.screens.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,12 +10,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 import com.spot.saver.android.R
 import com.spot.saver.android.presentation.ui.components.SavedSpotsHeader
@@ -27,6 +33,7 @@ import com.spot.saver.viewmodel.HomePageViewModel
 /**
  * Home page
  */
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomePage() {
 
@@ -36,6 +43,12 @@ fun HomePage() {
     }
 
     val mySpots by viewModel.savedSpots.collectAsState()
+
+    val lazyListState = rememberLazyListState()
+
+    val elevateSavedSpotsHeader by remember {
+        derivedStateOf { lazyListState.firstVisibleItemIndex >= 1 }
+    }
 
     LaunchedEffect(Unit) {
         viewModel.fetchMySpots()
@@ -49,30 +62,38 @@ fun HomePage() {
                 .fillMaxWidth()
                 .padding(20.dp),
             profilePicture = R.drawable.test_image,
-            onProfileClicked = {},
-            onSearchClicked = {}
+            onProfileClicked = {
+                // TODO: handle profile click event
+            },
+            onSearchClicked = {
+                // TODO: handle search button click event
+            }
         )
 
         LazyColumn(
+            state = lazyListState,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 20.dp)
         ) {
 
             item {
                 AddNewSpotButton(
                     modifier = Modifier
                         .padding(vertical = 8.dp)
+                        .padding(horizontal = 20.dp)
                 ) {
                     // TODO: handle add new button click event
                 }
             }
 
-            item {
+            stickyHeader {
                 SavedSpotsHeader(
-                    savedSpotsSize = 10,
+                    savedSpotsSize = mySpots.size,
                     sortedBy = "Expiring date",
                     modifier = Modifier
+                        .then(if (elevateSavedSpotsHeader) Modifier.shadow(8.dp) else Modifier)
+                        .background(MaterialTheme.colorScheme.surface)
+                        .padding(horizontal = 20.dp)
                         .padding(vertical = 12.dp)
                 ) {
                     // TODO: handle sort button click event
@@ -81,6 +102,8 @@ fun HomePage() {
 
             items(mySpots) { spot ->
                 SpotCardListItem(
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp),
                     spotDetails = spot,
                     onSpotClicked = {
                         // TODO: handle spot click event
